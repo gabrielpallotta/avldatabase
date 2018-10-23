66634779701d3a103f3e772d9ee35bd54746174e
gabrielpallotta <gabriel.cardenete@hotmail.com>
2018-10-23 07:57:17 -0300

    Initial commit

---
+++ include/avl_database.hpp
@@ -0,0 +1,9 @@
+#ifndef AVLDATABASE_H
+#define AVLDATABASE_H
+
+class AvlDatabase
+{
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "avl_database.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  cout << "Hello World" << endl;
+  
+  return 0;
+}
