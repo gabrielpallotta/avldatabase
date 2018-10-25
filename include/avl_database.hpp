f7a8143235e7da0bd6c3135a5cb41d8700b2c8fe
gabrielpallotta <gabriel.cardenete@hotmail.com>
2018-10-25 10:18:11 -0300

    Add simple binary file I/O

--- include/avl_database.hpp
+++ include/avl_database.hpp
@@ -1,8 +1,59 @@
 #ifndef AVLDATABASE_H
 #define AVLDATABASE_H
 
+#include <ios>
+#include <iostream>
+#include <fstream>
+
+typedef struct Node {
+  int key;
+  int index;
+  int left;
+  int right;
+} Node;
+
+template <typename K, typename T>
 class AvlDatabase
 {
+  public:
+    std::fstream data_file;
+    std::fstream tree_file;
+
+    AvlDatabase(std::string data_path, std::string tree_path) {
+      std::cout << sizeof(T);
+
+      data_file(data_path, std::ios_base::binary | std::ios_base::app);
+      tree_file(tree_path, std::ios_base::binary | std::ios_base::app);
+      // data_file.write((char*)&data, sizeof(data));
+      // T data;
+      // data_file.seekp(0);
+      // data_file.read((char*)&data, sizeof(data));
+      // std::cout << data.name << " " << data.ra;
+    }
+
+    void add (const K &key, const T &info) {
+      tree_file.seekp()
+      tree_file.seekp()
+    }
+
+    void remove (const K &key, const T &info) {
+      
+    }
+
+    bool has_info (const K &key, const T &info) {
+      
+    }
+
+  private:
+    Node read_node(streampos pos) {
+      Node node;
+      tree_file.seekp(pos);
+      tree_file.read(node.key, sizeof(int));
+      tree_file.read(node.index, sizeof(int));
+      tree_file.read(node.left, sizeof(int));
+      tree_file.read(node.right, sizeof(int))
+      return node;;
+    }
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
-#include "avl_database.hpp"
-
+#include <cstring>
 #include <iostream>
+
+#include "avl_database.hpp"
 
 using namespace std;
 
+typedef struct Student {
+  char name[20];
+  int ra;
+} Student;
+
 int main ()
 {
-  cout << "Hello World" << endl;
+  Student me;
+  strcpy(me.name, "Pallotta");
+  me.ra = 16168;
+
+  AvlDatabase<int, Student> students("data.bin", "tree.bin");
   
   return 0;
 }
